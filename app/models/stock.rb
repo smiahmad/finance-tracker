608ac43cfdd1d6052958b8354cee082002c7564f
Imran Ahmad <imran.ahmad@navis.com>
2022-10-08 14:28:38 -0700

    used Ajax for page refresh

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,20 @@
 class Stock < ApplicationRecord
+
   def self.new_lookup(ticker_symbol)
-    quote = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
-    quote.price
+    company = Alphavantage::Fundamental.new(symbol: ticker_symbol)
+    price_quote = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
+
+    if !company || company.overview.name == nil
+      # byebug
+      return nil
+    end
+
+    begin
+      new(ticker: ticker_symbol, name: company.overview.name, last_price: price_quote.price)
+    rescue => exception
+      # byebug
+      return nil
+    end
   end
+
 end
