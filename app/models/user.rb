0c0ead5eef332b39c9cb9010b8831d685765e2e2
Imran Ahmad <imran.ahmad@navis.com>
2022-10-10 17:22:30 -0700

    final commit of app with friends etc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -16,6 +18,16 @@ class User < ApplicationRecord
     stocks.where(id: stock.id).exists?
   end
 
+  def friend_already_tracked?(email)
+    friend = User.find_by(email: email)
+
+    #first check if stock even exists in the table
+    return false unless friend
+
+    # stock exists in the table. Now check if it related to the user
+    friend.where(id: friend.id).exists?
+  end
+
   def under_stock_limit?
     stocks.count < 10
   end
@@ -27,5 +39,40 @@ class User < ApplicationRecord
   def full_name
     return "#{first_name} #{last_name}" if first_name || last_name
     "Anonymous"
+  end
+
+  def can_track_friend?(email)
+    !friend_already_tracked?(email)
+  end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject{|user| user.id == self.id}
+  end
+
+  def not_friends_with?(friend_id)
+    !self.friends.where(id: friend_id).exists?
   end
 end
